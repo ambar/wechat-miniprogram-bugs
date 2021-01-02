9072fe0a0517cb2b54db1e1818e170cc809d2e27
ambar <ambar.lee@gmail.com>
2021-01-02 12:11:34 +0800

    chore: mv template to .template

--- .gogenrc.js
+++ .gogenrc.js
@@ -1,7 +1,7 @@
 module.exports = async ({src, dest, pipeline, template}, {name}) => {
   await pipeline(
     // copy files
-    src('template/**'),
+    src('.template/**'),
     template({name}),
     dest()
   )
